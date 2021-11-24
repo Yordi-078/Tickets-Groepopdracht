91c267d2ed499f8a20728e968267be510be80ba6
anthony <99057187@mydavinci.nl>
2021-11-24 09:48:51 +0100

    function name improved

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function toggleBoard() {
     var boards = document.getElementsByClassName("board");
     for (i = 0; i < boards.length; i++) {
         boards[i].classList.toggle('board-row');
