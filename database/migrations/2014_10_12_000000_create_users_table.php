50e9589104db2556cd39bcd31d5003d53a8c980d
BerkanKaya <99059255@mydavinci.nl>
2021-12-14 12:04:00 +0100

    user migration veranderd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->enum('user_role', ['student', 'teacher', 'admin'])->nullable();
+            $table->enum('user_role', ['student', 'teacher', 'admin'])->default('admin');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
