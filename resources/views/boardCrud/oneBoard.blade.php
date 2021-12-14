c648a970b1c4b182be92b2bcdad70569a77dd49a
Swen <99058148@mydavinci.nl>
2021-12-14 11:10:15 +0100

    Merge conflicts gefixed
    Update own card werkt nu met de goede school.

--- resources/views/boardCrud/oneBoard.blade.php
+++ resources/views/boardCrud/oneBoard.blade.php
@@ -7,16 +7,6 @@ $user_id = Auth::user()->id;
 
 ?>
 
-@section('addStudent')
-        <!-- this is what the user sees if user is admin or docent. 
-        this wil be added to all the other code and wil not replace it -->
-        @if (Auth::user()->user_role == 'teacher' || Auth::user()->user_role == 'admin' )
-        <a class="dropdown-item" href="{{ route('addStudentsToBoard', $thisBoard['id']) }}">
-            {{ __('Add students') }}
-        </a>
-        @endif
-@endsection
-
 @section('content')
 <div class="board-page-header">
     <!-- this is what the user sees if user is admin or docent. 
@@ -50,21 +40,36 @@ $user_id = Auth::user()->id;
                 <div class="modal-content">
                   <span id="close-popup" class="close">&times;</span>
                   
-                  <form action="{{ url('updateCard', [$card['id'], $card['board_id']]) }}" method="POST">
+                  <form  id="card-info-popup" action="{{ url('updateCard', [$card['id'], $card['board_id']]) }}" method="POST">
                       
                       @csrf
-                        <div id="general">
-                            <input type="text" name ="name" value="{{$card['name']}}" required>
-                            <input type="text" name="description" value="{{$card['description']}}" required>
+                      <div id="general" class="card-info-border">
+                            <span><i class="fas fa-align-left"></i></span>
+                            <textarea type="text" id="title" name="name" maxlength="300" required>{{$card['name']}}</textarea>
+                            <span>descrition: </span>
+                            <textarea type="text" id="description" name="description" maxlength="665" required>{{$card['description']}}</textarea>
                         </div>
-                        <div id="imageUploader">
+
+                        <div id="image-uploader" class="card-info-border">
                             <input type="file">
                         </div>
-                        <select name="status">
-                            <option value="{{$card['in_progress']}}">in progress</option>
-                            <option value="{{$card['finished']}}">finished</option>
-                        </select>
-                        <button type="submit">Submit</button>
+                        <div id="progress-info" class="card-info-border">
+                            <p>{{$card['created_at']}}</p>
+                            <select name="status">
+                                <option value="{{$card['in_progress']}}">in progress</option>
+                                <option value="{{$card['finished']}}">finished</option>
+                            </select>
+                        </div>
+                        <div id="helper-box" class="card-info-border">
+                            @if ($card['helper'])
+                                <input class="helper" type="button" value="{{$card['helper']}}">
+                            @else
+                            <input id="add-helper-button" type="button" onclick="addHelper('{{Auth::user()->id}}','{{Auth::user()->name}}' )" value=" + ">
+                            @endif
+                        </div>
+                        <div id="submit-form" class="card-info-border">
+                            <input type="submit" class="card-submit-button" value="submit">
+                        </div>
                   </form>
                 </div>
   
@@ -78,10 +83,23 @@ $user_id = Auth::user()->id;
                 <div class="modal-content">
                   <span class="close">&times;</span>
                   
-                  <form>
-                        <div id="general">
-                            <input type="text" value="{{$card['name']}}" required readonly>
-                            <input type="text" value="{{$card['description']}}" required readonly>
+                  <form id="card-info-popup">
+                  <div id="general" class="card-info-border">
+                            <span><i class="fas fa-align-left"></i></span>
+                            <textarea type="text" id="title" maxlength="300" readonly>{{$card['name']}}</textarea>
+                            <span>descrition: </span>
+                            <textarea type="text" id="description" maxlength="665" readonly>{{$card['description']}}</textarea>
+                        </div>
+
+                        <div id="image-uploader" class="card-info-border" readonly>
+                            <input type="file">
+                        </div>
+                        <div id="progress-info" class="card-info-border">
+                            <p>{{$card['created_at']}}</p>
+                            <select name="status" readonly>
+                                <option value="{{$card['in_progress']}}">in progress</option>
+                                <option value="{{$card['finished']}}">finished</option>
+                            </select>
                         </div>
                         <div id="helper-box" class="card-info-border">
                             @if ($card['helper'])
@@ -89,15 +107,9 @@ $user_id = Auth::user()->id;
                             @else
                             <input id="add-helper-button" type="button" onclick="addHelper('{{Auth::user()->id}}','{{Auth::user()->name}}' )" value=" + ">
                             @endif
-                        <div id="imageUploader">
-                            <input type="file" readonly>
                         </div>
-                        <select name="status">
-                            <option value="in_progress">in progress</option>
-                            <option value="finished">finished</option>
-                        </select>
-                        <input type="button" value="{{$card['helper']}}" readonly>
-                        <p>{{$card['created_at']}}</p>
+
+
                   </form>
                 </div>
   
