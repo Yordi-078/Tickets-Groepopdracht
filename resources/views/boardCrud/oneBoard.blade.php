949093ac55e39de8135509cb1bc1c690502373d1
Swen <99058148@mydavinci.nl>
2022-01-28 10:14:03 +0100

    Bugs fixed
    Je kan nu op het hele kaartje om een kaart te openen ipv alleen op het oogje en je kan de forms in een kaartje sluiten door op het kruisje te klikken.

--- resources/views/boardCrud/oneBoard.blade.php
+++ resources/views/boardCrud/oneBoard.blade.php
@@ -28,8 +28,8 @@ $user_id = Auth::user()->id;
     <div class="flex-row" id="board-question-content-box" >
         @foreach($cards as $card)
             
-            <a href="#" class="cards flex-row">
-                <button class="card-popup-button" onclick="showQuestionPopup('myModal{{$card['id']}}','{{$card['user_id']}}','{{$card['helper_id']}}','{{$card['id']}}','{{Auth::user()->id}}')">
+            <a onclick="showQuestionPopup('myModal{{$card['id']}}','{{$card['user_id']}}','{{$card['helper_id']}}','{{$card['id']}}','{{Auth::user()->id}}')" href="#" class="cards flex-row">
+                <button class="card-popup-button">
                     <i class="far fa-eye"></i>
                 </button>
                 {{$card->status == "finished" ? "//" : ''}}
@@ -43,7 +43,7 @@ $user_id = Auth::user()->id;
                 <!-- Modal content -->
                 <div class="modal-content">
                     <span id="card-owner" class="card-info-owner"></span>
-                    <span id="close-popup" class="close">&times;</span>
+                    <span id="close-popup" onclick="history.go(-0)" class="close">&times;</span>
                   
                   <form  id="card-info-popup" action="{{ url('updateCard', [$card['id'], $card['board_id']]) }}" method="POST">
                       
@@ -51,7 +51,7 @@ $user_id = Auth::user()->id;
                       <div id="general" class="card-info-border">
                             <span><i class="fas fa-align-left"></i></span>
                             <textarea type="text" id="title" name="name" maxlength="300" required>{{$card['name']}}</textarea>
-                            <span>descrition: </span>
+                            <span>description: </span>
                             <textarea type="text" id="description" name="description" maxlength="665" required>{{$card['description']}}</textarea>
                         </div>
 
@@ -94,7 +94,6 @@ $user_id = Auth::user()->id;
                 <div class="modal-content">
                 <span id="card-owner" class="card-info-owner"></span>
                   <span class="close">&times;</span>
-                  
                   <form id="card-info-popup">
                     @csrf
 
@@ -149,7 +148,7 @@ $user_id = Auth::user()->id;
         
         @foreach($lessonCards as $lessonCard)
  
-        <a href="#" class="toggle cards flex-row">{{$lessonCard["name"]}}<button class="card-popup-button" onclick="showPopup('myModalLesson{{$lessonCard['id']}}',{{$thisBoard['id']}})"><i class="far fa-eye"></i></button></a> 
+        <a onclick="showPopup('myModalLesson{{$lessonCard['id']}}',{{$thisBoard['id']}})" class="toggle cards flex-row">{{$lessonCard["name"]}}<button class="card-popup-button"><i class="far fa-eye"></i></button></a> 
         
         <!-- The Modal -->
 
@@ -160,7 +159,7 @@ $user_id = Auth::user()->id;
             <!-- Modal content -->
     
             <div class="modal-content">
-              <span class="close">&times;</span>
+              <span onclick="history.go(-1)" class="close">&times;</span>
               <form>
                   <a id="lesson-card-info-test">hello</a>
                 <a href="{{ url('storeLessonUpVote', [$lessonCard['id'], $thisBoard['id']]) }}" class="home-buttons">Upvote</a>
