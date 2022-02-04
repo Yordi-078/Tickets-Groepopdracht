7461015d226c43bb4a20ddf77037acb21f7cf1aa
Robin-Broekhof <99059076@mydavinci.nl>
2022-02-04 11:46:25 +0100

    teacher dashboard layout aangespast

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -97,8 +97,8 @@ class UserController extends Controller
      */
     public function teacherDashboard(User $board_id)
     {
-        //$cards = Card::all();
-        $cards = Card::all()->where('status', 'finished')->where("helper_id", auth()->id())->sortBy('updated_at');
+        $cards = Card::all();
+        //$cards = Card::all()->where('status', 'finished')->where("helper_id", auth()->id())->sortBy('updated_at');
         return view('teacherDashboard.index', compact('cards', 'board_id'));
     }
 
