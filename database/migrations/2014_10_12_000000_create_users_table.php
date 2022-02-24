ab03c33cb94241d1d968dab4279563b954e5f0ed
BerkanKaya <99059255@mydavinci.nl>
2022-02-24 10:40:18 +0100

    heroku

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,22 +6,14 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+  
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('email')->unique()->nullable();
-<<<<<<< HEAD
-            $table->string('user_role')->default('student');
-=======
             $table->unsignedBigInteger('user_role_id')->default(1);
->>>>>>> main
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken()->nullable();
@@ -29,11 +21,6 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
