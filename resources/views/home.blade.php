3f3a94f9c26bb709f667371d73fa34fedb6ad2b0
Yordi <99056993@mydavinci.nl>
2022-02-24 10:35:09 +0100

    user_role

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <button id="toggle-board" class="home-buttons" onclick="toggleBoard()"><i class="fas fa-bars"></i></button>
     <!-- this is what the user sees if user is admin or docent. 
     this wil be added to all the other code and wil not replace it -->
-    @if (Auth::user()->user_role == 'teacher' || Auth::user()->user_role == 'admin' )
+    @if (Auth::user()->user_role_id == 2 || Auth::user()->user_role_id == 3 )
     <div class="home-buttons"><a class=" create-board-button " href="{{ route('createBoard') }}">
         {{ __('create board') }}
     </a></div>
