285a73cca51b58c452dd0ae1d646bbf0e2c031a2
Yordi <99056993@mydavinci.nl>
2022-03-10 10:00:58 +0100

    middleware van login naar welcome

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,4 +1,4 @@
-<?php
+
 
 namespace App\Http\Middleware;
 
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('/');
         }
     }
 }
