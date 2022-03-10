ff8916715f4dc9df05b92a25e5df8409a89391bf
Anthony <99057187@mydavinci.nl>
2022-03-10 09:15:23 +0100

    all form pages are modalled to fit at the 3 main sizes.

--- resources/views/boardCrud/createLessonCard.blade.php
+++ resources/views/boardCrud/createLessonCard.blade.php
@@ -6,29 +6,29 @@
         <div class="form-header-image"><i class="fa fa-clipboard-list"></i></div>
         <h1>maak een lesson card aan</h1>
     </div>
-    <form method="POST" action="{{ url('storeLessonCard', $board_id) }}">
+    <form class="make-card-form" method="POST" action="{{ url('storeLessonCard', $board_id) }}">
     @csrf
-        <div>
+        <div class="">
             <label class="label" for="name">name: </label>    
-            <input class="input @error('name') is-danger @enderror" type="text" name="name" id="" value="{{ old('name') }}">
+            <input class="form-input-container input @error('name') is-danger @enderror" type="text" name="name" id="" value="{{ old('name') }}">
             
             @error('name')
                 <p class="help is-danger">{{ $errors->first('name') }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="">
             <label class="label" for="description">description: </label>  
-            <input class="input @error('description') is-danger @enderror" type="text" name="description" id="" value="{{ old('description') }}">
+            <textarea class="form-input-container input @error('description') is-danger @enderror" type="text" name="description" id="" maxlength="69" value="{{ old('description') }}"></textarea>
         
             @error('description')
                 <p class="help is-danger">{{ $errors->first('description') }}</p>
             @enderror
         </div>
 
-        <div>
+        <div class="">
             <label class="label" for="start_time">start time: </label>    
-            <input class="input @error('description') is-danger @enderror" type="datetime-local" name="start_time" id="" value="{{ old('start_time') }}">
+            <input class="form-input-container input @error('description') is-danger @enderror" type="datetime-local" name="start_time" id="" value="{{ old('start_time') }}">
             
             @error('description')
                 <p class="help is-danger">{{ $errors->first('start_time') }}</p>
