61d8bd5c26aa9fe2f9020e7340b450f74b2ca134
Robin-Broekhof <99059076@mydavinci.nl>
2022-03-25 03:35:24 +0100

    teacher dashboard calander layout af

--- resources/views/teacherDashboard/index.blade.php
+++ resources/views/teacherDashboard/index.blade.php
@@ -4,119 +4,69 @@
     <script src="{{ asset('js/calendar_script.js') }}"></script>
 @endsection
 
-<style>
-
-    .calendar {
-        position: relative;
-        width: 300px;
-        background-color: #fff;
-        box-sizing: border-box;
-        box-shadow: 0 5px 50px rgba(#000, 0.5);
-        border-radius: 8px;
-    }
-
-    .calendar__date {
-        padding: 20px;
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(25px, 1fr));
-        grid-gap: 10px;
-        box-sizing: border-box;
-    }
-
-    .calendar__day {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 25px;
-        font-weight: 600;
-        color: #262626;
-        
-        &:nth-child(7) {
-            color: #ff685d;
-        }
-    }
-
-    .calendar__number {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 25px;
-        color: #262626;
-
-        &:nth-child(7n) {
-            color: #ff685d;
-            font-weight: 700;
-        }
-    
-        &--current,
-        &:hover {
-            background-color: #009688;
-            color: #fff !important;
-            font-weight: 700;
-            cursor: pointer;
-        }
-    }
-</style>
-
 @php
     use Illuminate\support\facades\Auth;
     $user_id = Auth::user()->id;
     $array = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-    
+    $dateFormat = false;
     $calendar = Carbon\Carbon::now();
-
     $firstDay = Carbon\Carbon::now()->startOfMonth();
-
     $lastDay = Carbon\Carbon::now()->lastOfMonth();
     $countDaysInMonth = $calendar->daysInMonth;
     $calendarMonth = $calendar->isoFormat('MMMM');
-
 @endphp
 
 @section('content')
 
 
 @foreach ($cards as $card)
-        <a>
-            {{ $card->updated_at->isoFormat('DD-MM-YYYY') }}
-        </a>
-        <br>
+    <a>{{ $card->updated_at->isoFormat('DD-MM-YYYY') }}</a>
 @endforeach
 
 @foreach ($lessonCards as $lessonCard)
-    <a>
-        {{ $lessonCard->name }}
-    </a>
+    <a>{{ $lessonCard->name }}</a>
 @endforeach
 
-<br>
 
 
 
 
 <div class="calendar">
-    <button id="previousMonth"> <----- </button>
+    <div class="calendar-top-row">
+        <button btn id="previousMonth"> <----- </button>
         <a>{{ $calendarMonth }}</a>
-    <button btn id="nextMonth"> -----> </button>
+        <button btn id="nextMonth"> -----> </button>
+    </div>
     
-    <div class="calendar__date">
+    <div class="calendar-date">
         @for ($i = 0; $i < 7; $i++)
-            <div class="calendar__day">
+            <div class="calendar-day">
                 <a>{{ $array[$i] }}</a>
             </div>
         @endfor
         @for ($i = 0; $i < $countDaysInMonth; $i++)
-            <div class="calendar__number">
+            @if($dateFormat == false)
+                @if($firstDay->isoFormat('dd') == $array[$i])
+                    @php
+                        $dateFormat = true
+                    @endphp
+                @else
+                    <div class="calendar-number"></div>
+                @endif
+            @endif
+
+            <div class="calendar-number">
                 <a href="{{ route('dateSelected', [$board_id , $firstDay->isoFormat('DD-MM-YYYY')]) }}" {{ $firstDay->isoFormat('DD-MM-YYYY') == $calendar->isoFormat('DD-MM-YYYY') ? "class=calendar-today"  : ''}} > {{ $firstDay->isoFormat('DD') }} </a>
             </div>
-            @php
-                $firstDay->add(1, 'day')       
-            @endphp
+
+            @if ($dateFormat !== false)
+                @php
+                    $firstDay->add(1, 'day')       
+                @endphp
+            @endif
         @endfor
-
-
     </div>
-  </div>
+</div>
 
 
 
--- resources/views/teacherDashboard/index2.blade.php
+++ resources/views/teacherDashboard/index2.blade.php
@@ -7,15 +7,13 @@
 @php
     use Illuminate\support\facades\Auth;
     $user_id = Auth::user()->id;
-    
+    $array = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+    $dateFormat = false;
     $calendar = Carbon\Carbon::now();
-
     $firstDay = Carbon\Carbon::now()->startOfMonth();
-
     $lastDay = Carbon\Carbon::now()->lastOfMonth();
     $countDaysInMonth = $calendar->daysInMonth;
     $calendarMonth = $calendar->isoFormat('MMMM');
-
 @endphp
 
 @section('content')
@@ -31,30 +29,41 @@
     
 @endforeach
 
-<br><br><br>
-
-<div>
-    <div class='top-calendar'>
-        <a class="lastMonth"> <----- </a>
+<div class="calendar">
+    <div class="calendar-top-row">
+        <button btn id="previousMonth"> <----- </button>
         <a>{{ $calendarMonth }}</a>
-        <a class="nextMonth"> -----> </a>
+        <button btn id="nextMonth"> -----> </button>
     </div>
-    <br>
-    @for ($i = 0; $i < $countDaysInMonth; $i++)
-        @if ($i == 7 OR $i == 14 OR $i == 21 OR $i == 28)
-            <br>
-        @endif
+    
+    <div class="calendar-date">
+        @for ($i = 0; $i < 7; $i++)
+            <div class="calendar-day">
+                <a>{{ $array[$i] }}</a>
+            </div>
+        @endfor
+        @for ($i = 0; $i < $countDaysInMonth; $i++)
+            @if($dateFormat == false)
+                @if($firstDay->isoFormat('dd') == $array[$i])
+                    @php
+                        $dateFormat = true
+                    @endphp
+                @else
+                    <div class="calendar-number"></div>
+                @endif
+            @endif
 
+            <div class="calendar-number">
+                <a href="{{ route('dateSelected', [$board_id , $firstDay->isoFormat('DD-MM-YYYY')]) }}" {{ $firstDay->isoFormat('DD-MM-YYYY') == $calendar->isoFormat('DD-MM-YYYY') ? "class=calendar-today"  : ''}} > {{ $firstDay->isoFormat('DD') }} </a>
+            </div>
 
-        <a href="{{ route('dateSelected', [$board_id , $firstDay->isoFormat('DD-MM-YYYY')]) }}" {{ $firstDay->isoFormat('DD-MM-YYYY') == $calendar->isoFormat('DD-MM-YYYY') ? "class=calendar-today"  : ''}} > {{ $firstDay->isoFormat('DD') }} </a>
-
-        <a> - </a>
-
-
-        @php
-            $firstDay->add(1, 'day')       
-        @endphp
-    @endfor
+            @if ($dateFormat !== false)
+                @php
+                    $firstDay->add(1, 'day')       
+                @endphp
+            @endif
+        @endfor
+    </div>
 </div>
 
 
