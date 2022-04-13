c07affab416e81548a79bd6a0fa59c8a403692d4
BerkanKaya <99059255@mydavinci.nl>
2022-04-13 13:05:38 +0200

    Layout gemaakt voor alle Reviews page

--- resources/views/review/allReviews.blade.php
+++ resources/views/review/allReviews.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($reviews as $review)
-    <div class="reviews">
-        <p class="review"> {{$review["text"]}} </p>
+
+<div class="main-section">
+    <div class="hedding-title"><h1>Les Reviews</h1></div>
+        <div class="rating-part">
+        <div class="average-rating">
     </div>
-    @endforeach
+    
+<div style="clear: both;"></div>
+<div class="reviews"><h1>Reviews</h1></div>
+@foreach($reviews as $review)
+<div class="comment-part">
+<div class="user-img-part">
+<div class="user-img">
+<img src="">
+
+</div>
+<div class="user-text">
+<p>User</p>
+
+</div>
+<div style="clear: both;"></div>
+</div>
+
+<div class="comment">
+<i id="neutral" class="fa-regular fa-face-meh fa-2x"></i>
+<p>{{$review->text}}</p>
+
+</div>
+
+<div style="clear: both;"></div>
+</div>
+
+@endforeach
 @endsection
