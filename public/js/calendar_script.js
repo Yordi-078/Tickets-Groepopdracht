5f33836077fce4a53b29dcc8d3354776c453e594
Ye Xiu <99059076@mydavinci.nl>
2022-06-09 19:14:34 +0200

    jquery komt op pagina

--- public/js/calendar_script.js
+++ public/js/calendar_script.js
@@ -1,25 +1,2 @@
-const ele0 = document.getElementById('previous-month');
-const ele1 = document.getElementById('next-month');
 
-const calendarPopUp = document.getElementById('calendar_popup');
-const calendarContent = document.getElementById('calendar-content');
-let calendarShow = false;
-
-ele0.onclick = function() {
-  ele0.style.display = "none";
-}
-
-ele1.onclick = function() {
-  ele1.style.display = "none";
-}
-
-calendarPopUp.onclick = function() {
-  if(calendarShow == false){
-    calendarContent.style.display = "block";
-    calendarShow = true;
-  }
-  else{
-    calendarContent.style.display = "none";
-    calendarShow = false;
-  }
-}
+       
